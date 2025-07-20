6a31f589db440a304c518b1b6858bfae51c5cebb
Eigeen <dengyk2002@qq.com>
2025-07-20 20:06:13 +0800

    fix identifier

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -15,7 +15,7 @@ if (keystorePropertiesFile.exists()) {
 }
 
 android {
-    namespace = "net.e1547"
+    namespace = "com.eigeen.e1547"
     compileSdk = flutter.compileSdkVersion
     ndkVersion = "27.0.12077973" // flutter.ndkVersion
 
@@ -30,7 +30,7 @@ android {
     }
 
     defaultConfig {
-        applicationId = "net.e1547"
+        applicationId = "com.eigeen.e1547"
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
         minSdk = flutter.minSdkVersion
